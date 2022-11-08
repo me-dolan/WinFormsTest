ef90d7600f9bb0cea8f80c87d8ca5770edc1ed1e
Пинчук Павел <pinchuk-2001@mail.ru>
2022-11-08 13:58:50 +0700

    Добавьте файлы проекта.

---
+++ WinFormsTest/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace WinFormsTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.downloadButton = new System.Windows.Forms.Button();
+            this.ReadTxtButton = new System.Windows.Forms.Button();
+            this.Zip = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // downloadButton
+            // 
+            this.downloadButton.Location = new System.Drawing.Point(12, 12);
+            this.downloadButton.Name = "downloadButton";
+            this.downloadButton.Size = new System.Drawing.Size(79, 26);
+            this.downloadButton.TabIndex = 0;
+            this.downloadButton.Text = "Download";
+            this.downloadButton.UseVisualStyleBackColor = true;
+            this.downloadButton.Click += new System.EventHandler(this.downloadButton_Click);
+            // 
+            // ReadTxtButton
+            // 
+            this.ReadTxtButton.Location = new System.Drawing.Point(337, 12);
+            this.ReadTxtButton.Name = "ReadTxtButton";
+            this.ReadTxtButton.Size = new System.Drawing.Size(80, 26);
+            this.ReadTxtButton.TabIndex = 2;
+            this.ReadTxtButton.Text = "Read";
+            this.ReadTxtButton.UseVisualStyleBackColor = true;
+            this.ReadTxtButton.Click += new System.EventHandler(this.ReadTxtButton_Click);
+            // 
+            // Zip
+            // 
+            this.Zip.Location = new System.Drawing.Point(705, 12);
+            this.Zip.Name = "Zip";
+            this.Zip.Size = new System.Drawing.Size(83, 26);
+            this.Zip.TabIndex = 3;
+            this.Zip.Text = "Zip";
+            this.Zip.UseVisualStyleBackColor = true;
+            this.Zip.Click += new System.EventHandler(this.Zip_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 30);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Скачать и \r\nразархивировать файл";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(261, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(261, 30);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Обрабатывает txt файл и записывает\r\n их на 3 равные csv. Занимает где то секунд 2" +
+    "0\r\n";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(651, 41);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(137, 30);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Создаёт архив\r\n с 3-мя созданными csv\r\n";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 198);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Zip);
+            this.Controls.Add(this.ReadTxtButton);
+            this.Controls.Add(this.downloadButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button downloadButton;
+        private Button ReadTxtButton;
+        private Button Zip;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+    }
+}
---
+++ WinFormsTest/Form1.cs
@@ -0,0 +1,232 @@
+using System.Diagnostics;
+using System.Globalization;
+using System.IO.Compression;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Text;
+
+namespace WinFormsTest
+{
+    public partial class Form1 : Form
+    {
+
+        private readonly HttpClient _httpClient;
+        private readonly string _directory = Directory.GetCurrentDirectory();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            _httpClient = new HttpClient() ;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void downloadButton_Click(object sender, EventArgs e)
+        {
+            await downloadFile();
+            unzipFile(_directory + "\\download\\dw.rar");
+        }
+
+        private void ReadTxtButton_Click(object sender, EventArgs e)
+        {
+            readFile(_directory + "\\unzip\\48H.txt");
+        }
+
+        private void Zip_Click(object sender, EventArgs e)
+        {
+            zipFile();
+        }
+
+        private async Task downloadFile()
+        {
+            var downloadUri = new Uri("https://www.autoprice.stutzen.ru/price?download=52005925-84b8-410f-88e6-cd2ee825292a");
+            using (HttpResponseMessage response = _httpClient.GetAsync(downloadUri, HttpCompletionOption.ResponseHeadersRead).Result)
+            {
+                response.EnsureSuccessStatusCode();
+                Directory.CreateDirectory(_directory + "\\download");
+                using (Stream stream = await response.Content.ReadAsStreamAsync(),//download/dw.rar"
+                    fileStream = new FileStream(_directory + "\\download\\dw.rar", FileMode.Create, FileAccess.Write, FileShare.None, 8192, true))
+                {
+                    // "download/dw.zip"
+                    var totalRead = 0L;
+                    var totalReads = 0L;
+                    var buffer = new byte[8192];
+                    var isMoreToRead = true;
+
+                    do
+                    {
+                        var read = await stream.ReadAsync(buffer, 0, buffer.Length);
+                        if (read == 0)
+                        {
+                            isMoreToRead = false;
+                        }
+                        else
+                        {
+                            await fileStream.WriteAsync(buffer, 0, read);
+
+                            totalRead += read;
+                            totalReads += 1;
+                        }
+                    }
+                    while (isMoreToRead);
+
+                }
+            }
+
+            MessageBox.Show("Файл скачался");
+        }
+
+        private void unzipFile(string pathToFile)
+        {
+            if (!File.Exists(pathToFile))
+            {
+                return;
+            }
+            Directory.CreateDirectory(_directory + "\\unzip");
+            string pathToUnzip = _directory + "\\unzip";
+            Process process = new Process();
+            //ProcessStartInfo ps = new ProcessStartInfo();
+            process.StartInfo.FileName = @"C:\Program Files\WinRAR\UnRAR";
+            process.StartInfo.Arguments = $"x {pathToFile} {pathToUnzip}";
+            process.Start();
+            process.WaitForExit();
+        }
+
+        private void readFile(string pathToFile)
+        {
+            List<string> allLine = new List<string>();
+            //mose[24] - 25 элемент   EKAT[23]
+            var fileLine = File.ReadAllLines(pathToFile, Encoding.GetEncoding(1251));
+
+            string firstLine = fileLine[0];
+
+            List<string> list1 = new List<string>() { "sep=", firstLine };
+            List<string> list2 = new List<string>() { "sep=", firstLine };
+            List<string> list3 = new List<string>() { "sep=", firstLine };
+
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            Parallel.ForEach(fileLine, chunk =>
+            {
+                var splitLine = chunk.Split("\t").ToList();
+                splitLine.RemoveAt(24);
+                if (double.TryParse(splitLine[23], NumberStyles.Float, new CultureInfo("en-US"), out double result))
+                {
+                    result = result * 67;
+                    splitLine[23] = result.ToString();
+                }
+
+                string rdyLine = string.Join('\t', splitLine);
+                if (string.IsNullOrEmpty(firstLine))
+                {
+                    firstLine = rdyLine;
+                }
+                allLine.Add(rdyLine);
+                splitLine.Clear();
+            });
+            stopwatch.Stop();
+            //foreach (var chunk in fileLine)
+            //{
+            //    Parallel.ForEach(chunk, (item) =>
+            //    {
+            //        var splitLine = item.ToString().Split("\t").ToList();
+            //        splitLine.RemoveAt(24);
+            //        if (double.TryParse(splitLine[23], NumberStyles.Float, new CultureInfo("en-US"), out double result))
+            //        {
+            //            result = result * 67;
+            //            splitLine[23] = result.ToString();
+            //        }
+
+            //        string rdyLine = string.Join('\t', splitLine);
+            //        if (string.IsNullOrEmpty(firstLine))
+            //        {
+            //            firstLine = rdyLine;
+            //        }
+            //        allLine.Add(rdyLine);
+            //        splitLine.Clear();
+            //    });
+            //}
+
+            //stopwatch.Stop();
+
+            //stopwatch.Start();
+            //foreach (var item in fileLine)
+            //{
+            //    var splitLine = item.Split("\t").ToList();
+            //    splitLine.RemoveAt(24);
+            //    if (double.TryParse(splitLine[23], NumberStyles.Float, new CultureInfo("en-US"), out double result))
+            //    {
+            //        result = result * 67;
+            //        splitLine[23] = result.ToString();
+            //    }
+
+            //    string rdyLine = string.Join('\t', splitLine);
+            //    if (string.IsNullOrEmpty(firstLine))
+            //    {
+            //        firstLine = rdyLine;
+            //    }
+            //    allLine.Add(rdyLine);
+            //    splitLine.Clear();
+            //}
+
+            //stopwatch.Stop();
+
+            allLine.RemoveAt(0);
+            allLine.Sort();
+            //List<string> list1 = new List<string>() { "sep=" ,firstLine };
+            //List<string> list2 = new List<string>() { "sep=", firstLine };
+            //List<string> list3 = new List<string>() { "sep=", firstLine };
+
+            foreach (var chunk in allLine.Chunk(3))
+            {
+                try
+                {
+                    list1.Add(chunk[0].ToString());
+                    list2.Add(chunk[1].ToString());
+                    list3.Add(chunk[2].ToString());
+                }
+                catch (Exception)
+                {
+                    string pathToOut1 = _directory + "\\download\\outpute1.txt";
+                    string pathToOut2 = _directory + "\\download\\outpute2.txt";
+                    string pathToOut3 = _directory + "\\download\\outpute3.txt";
+
+                    File.WriteAllLines(pathToOut1, list1.ToArray(), Encoding.GetEncoding(1251));
+                    File.Move(pathToOut1, Path.ChangeExtension(pathToOut1, ".csv"));
+
+                    File.WriteAllLines(pathToOut3, list2.ToArray(), Encoding.GetEncoding(1251));
+                    File.Move(pathToOut3, Path.ChangeExtension(pathToOut2, ".csv"));
+
+                    File.WriteAllLines(pathToOut3, list3.ToArray(), Encoding.GetEncoding(1251));
+                    File.Move(pathToOut3, Path.ChangeExtension(pathToOut3, ".csv"));
+                }
+            }
+
+            MessageBox.Show("Файлы csv успешно созданы");
+        }
+
+        private void zipFile()
+        {
+
+            Directory.CreateDirectory(_directory + "\\download\\zip");
+            string pathToZip = _directory + "\\download\\zip\\zip.rar";
+            string file1 = _directory + "\\download\\outpute1.csv";
+            string file2 = _directory + "\\download\\outpute2.csv";
+            string file3 = _directory + "\\download\\outpute3.csv";
+
+            Process process = new Process();
+            //ProcessStartInfo ps = new ProcessStartInfo();
+            process.StartInfo.FileName = @"C:\Program Files\WinRAR\Rar";
+            process.StartInfo.Arguments = $"a -ep {pathToZip} {file1} {file2} {file3}";
+            process.Start();
+            process.WaitForExit();
+            MessageBox.Show("Архив создан в папке загрузки zip архива");
+        }
+
+    }
+}
